cd52e185c71535d470a6a5a3be5a329b0e752371
Yeswanth <swamiyeswanth@gmail.com>
2018-11-29 21:32:10 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,61 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+
+export default class App extends Component<Props> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      randomNumber: 0
+    }
+  }
+
+  getRandomNumber = () => {
+    console.log('Call your native module here')
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.instructions}>Press button to get a random number</Text>
+        <Button title={"Super complex random number"} onPress={this.getRandomNumber} />
+        <Text style={styles.randomNumber}>{`Number: ${this.state.randomNumber}`}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'wheat',
+  },
+  welcome: {
+    fontSize: 15,
+    textAlign: 'center',
+    margin: 20,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 20,
+  },
+  randomNumber: {
+    marginTop: 40,
+    fontSize: 30,
+    textAlign: 'center',
+    color: 'red',
+    backgroundColor: 'white',
+    padding: 10
+  }
+});
